906b3e7dcd323a8a6b990dbfa54217a9fc77d8e1
Sabin Pruna <sabin-catalin.pruna@student.unitbv.ro>
2018-04-02 14:52:05 +0300

    Added Graphs project.

---
+++ Graphs/GraphUtils/GraphTypeEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace Graphs.GraphUtils {
+    public enum GraphTypeEnum {
+        Complete = 0,
+        Empty = 1,
+        Random = 2
+    }
+}
---
+++ Graphs/Program.cs
@@ -0,0 +1,19 @@
+﻿using Graphs.GraphUtils;
+
+namespace Graphs {
+    internal class Program {
+        private static void Main(string[] args) {
+
+
+
+            Graph graph = new Graph(GraphTypeEnum.Empty, 10);
+
+            Graph graph2 = new Graph(GraphTypeEnum.Complete, 10);
+
+            Graph graph3 = new Graph(GraphTypeEnum.Random, 10);
+
+
+
+        }
+    }
+}
